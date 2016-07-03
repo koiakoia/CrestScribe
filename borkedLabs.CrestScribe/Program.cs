072265bcc3ab29f4f2687985c39288b2b19b82c5
Mark Roszko <mark.roszko@gmail.com>
2016-07-03 00:45:20 -0400

    Add service install/uninstall management

--- borkedLabs.CrestScribe/Program.cs
+++ borkedLabs.CrestScribe/Program.cs
@@ -1,25 +1,50 @@
-﻿using System.Threading;
+﻿using System;
+using System.Collections;
+using System.ServiceProcess;
+using System.Threading;
+using System.Configuration.Install;
 
 namespace borkedLabs.CrestScribe
 {
     static class Program
     {
+        private const string serviceName = "CrestScribe";
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        static void Main()
+        static void Main(string[] args)
         {
             ScribeSettings.Load("settings.json");
 
             //Because VS cannot debug services, we instead change this section to compile
             //the actual service in and out depending on configuration
 #if (!DEBUG)
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[]
+            if (args.Length == 0)
             {
-                new ScribeService()
-            };
-            ServiceBase.Run(ServicesToRun);
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[]
+                {
+                    new ScribeService()
+                };
+                ServiceBase.Run(ServicesToRun);
+            }
+            else if (args.Length == 1)
+            {
+                switch (args[0])
+                {
+                    case "-install":
+                        InstallService();
+                        StartService();
+                        break;
+                    case "-uninstall":
+                        StopService();
+                        UninstallService();
+                        break;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
 #else
             var service = new ScribeService();
             service.StartWork();
@@ -30,5 +55,143 @@ namespace borkedLabs.CrestScribe
             }
 #endif
         }
+
+        #region ServiceManagement
+
+        private static bool IsInstalled()
+        {
+            using (ServiceController controller =
+                new ServiceController(serviceName))
+            {
+                try
+                {
+                    ServiceControllerStatus status = controller.Status;
+                }
+                catch
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        private static bool IsRunning()
+        {
+            using (ServiceController controller =
+                new ServiceController(serviceName))
+            {
+                if (!IsInstalled()) return false;
+                return (controller.Status == ServiceControllerStatus.Running);
+            }
+        }
+
+        private static AssemblyInstaller GetInstaller()
+        {
+            AssemblyInstaller installer = new AssemblyInstaller(
+                typeof(ScribeService).Assembly, null);
+            installer.UseNewContext = true;
+            return installer;
+        }
+
+        private static void InstallService()
+        {
+            if (IsInstalled()) return;
+
+            try
+            {
+                using (AssemblyInstaller installer = GetInstaller())
+                {
+                    IDictionary state = new Hashtable();
+                    try
+                    {
+                        installer.Install(state);
+                        installer.Commit(state);
+                    }
+                    catch
+                    {
+                        try
+                        {
+                            installer.Rollback(state);
+                        }
+                        catch { }
+                        throw;
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private static void UninstallService()
+        {
+            if (!IsInstalled()) return;
+            try
+            {
+                using (AssemblyInstaller installer = GetInstaller())
+                {
+                    IDictionary state = new Hashtable();
+                    try
+                    {
+                        installer.Uninstall(state);
+                    }
+                    catch
+                    {
+                        throw;
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private static void StartService()
+        {
+            if (!IsInstalled()) return;
+
+            using (ServiceController controller =
+                new ServiceController(serviceName))
+            {
+                try
+                {
+                    if (controller.Status != ServiceControllerStatus.Running)
+                    {
+                        controller.Start();
+                        controller.WaitForStatus(ServiceControllerStatus.Running,
+                            TimeSpan.FromSeconds(10));
+                    }
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+        }
+
+        private static void StopService()
+        {
+            if (!IsInstalled()) return;
+            using (ServiceController controller =
+                new ServiceController(serviceName))
+            {
+                try
+                {
+                    if (controller.Status != ServiceControllerStatus.Stopped)
+                    {
+                        controller.Stop();
+                        controller.WaitForStatus(ServiceControllerStatus.Stopped,
+                             TimeSpan.FromSeconds(10));
+                    }
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+        }
+        #endregion
     }
 }
